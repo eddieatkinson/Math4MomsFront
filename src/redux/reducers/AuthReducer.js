a7a49fa6be70681c2cb285147402adce0b06b109
Eddie Atkinson <eddiebatkinson@gmail.com>
2020-04-06 10:00:52 -0400

    boilerplate finished with redux and router

---
+++ src/redux/reducers/AuthReducer.js
@@ -0,0 +1,12 @@
+import {} from "../types";
+
+const INITIAL_STATE = {
+  name: "",
+};
+
+export default function (state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
---
+++ src/utilities/index.js
@@ -0,0 +1,5 @@
+export const myYellow = "#F8E9A1";
+export const myRed = "#F76C6C";
+export const myLightBlue = "#A8D0E6";
+export const myBlue = "#374785";
+export const myDarkBlue = "#24305E";
